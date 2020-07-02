aab94caf73415a35017fedc2bf2ec3a90855c046
bannzai <kingkonog999yhirose@gmail.com>
2020-07-03 01:12:50 +0900

    :sparkels: add client error pattern

--- Tests/XChangerTests/XChangerTests.swift
+++ Tests/XChangerTests/XChangerTests.swift
@@ -5,31 +5,59 @@ struct Coder: Codable, Equatable {
     let id: Int
     let name: String
 }
+
+struct MyError: Error {
+    
+}
 final class XChangerTests: XCTestCase {
     func testExample() {
-        XChanger.register()
-        defer { XChanger.unregister() }
-
-        let expect = expectation(description: #function)
-        let json = try! JSONEncoder().encode(Coder(id: 10, name: "bannzai"))
-        XChanger.add(
-            XChanger.exchange().request(url: "/").response(data: json, statusCode: 200)
-        )
-        
-        let request = URLRequest(url: URL(string: "/")!)
-        let session = URLSession(configuration: URLSessionConfiguration.default)
-        session.dataTask(with: request) { data, response, error in
-            guard let httpResponse = response as? HTTPURLResponse else {
-                fatalError()
-            }
-            XCTAssertEqual(httpResponse.statusCode, 200)
-
-            let decoded = try! JSONDecoder().decode(Coder.self, from: data!)
-            XCTAssertEqual(decoded, Coder(id: 10, name: "bannzai"))
-   
-            expect.fulfill()
-        }.resume()
-        
-        wait(for: [expect], timeout: 1)
+        XCTContext.runActivity(named: "Success reponse pattern") { (_) in
+            XChanger.register()
+            defer { XChanger.unregister() }
+            
+            let expect = expectation(description: #function)
+            let json = try! JSONEncoder().encode(Coder(id: 10, name: "bannzai"))
+            XChanger.add(
+                XChanger.exchange().request(url: "/").response(data: json, statusCode: 200)
+            )
+            
+            let request = URLRequest(url: URL(string: "/")!)
+            let session = URLSession(configuration: URLSessionConfiguration.default)
+            session.dataTask(with: request) { data, response, error in
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    fatalError()
+                }
+                XCTAssertEqual(httpResponse.statusCode, 200)
+                
+                let decoded = try! JSONDecoder().decode(Coder.self, from: data!)
+                XCTAssertEqual(decoded, Coder(id: 10, name: "bannzai"))
+                
+                expect.fulfill()
+            }.resume()
+            
+            wait(for: [expect], timeout: 1)
+        }
+        XCTContext.runActivity(named: "client error reponse pattern") { (_) in
+            XChanger.register()
+            defer { XChanger.unregister() }
+            
+            let expect = expectation(description: #function)
+            XChanger.add(
+                XChanger.exchange().request(url: "/").response(error: .client(MyError()))
+            )
+            
+            let request = URLRequest(url: URL(string: "/")!)
+            let session = URLSession(configuration: URLSessionConfiguration.default)
+            session.dataTask(with: request) { data, response, error in
+                if response != nil, data != nil {
+                   XCTFail("Unexpected response and data are not nil")
+                }
+                
+                XCTAssertTrue(error is MyError)
+                expect.fulfill()
+            }.resume()
+            
+            wait(for: [expect], timeout: 1)
+        }
     }
 }
