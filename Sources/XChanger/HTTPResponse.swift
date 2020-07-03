abfe953b7fb6306a46d9797b91f6d8d0853407b4
bannzai <kingkong999yhirose@gmail.com>
2020-07-03 23:34:31 +0900

    :recycle: Move to http for request and response

--- Sources/XChanger/HTTPResponse.swift
+++ Sources/XChanger/HTTPResponse.swift
@@ -2,24 +2,7 @@ import Foundation
 
 public var defaultCacheStoragePolicy: URLCache.StoragePolicy = URLCache.StoragePolicy.allowed
 
-public struct HTTPResponse {
-    internal let result: Result<(data: Data, response: HTTPURLResponse), ResponseError>
-    internal let cacheStoragePolicty: URLCache.StoragePolicy
-    
-    public init(success: (data: Data, response: HTTPURLResponse), cacheStoragePolicty: URLCache.StoragePolicy = defaultCacheStoragePolicy) {
-        self.result = .success(success)
-        self.cacheStoragePolicty = cacheStoragePolicty
-    }
-    public init(error: ResponseError, cacheStoragePolicty: URLCache.StoragePolicy = defaultCacheStoragePolicy) {
-        self.result = .failure(error)
-        self.cacheStoragePolicty = cacheStoragePolicty
-    }
-    public init(error: Error, response: URLResponse? = nil, cacheStoragePolicty: URLCache.StoragePolicy = defaultCacheStoragePolicy) {
-        self.result = .failure(ResponseError(error: error, response: response))
-        self.cacheStoragePolicty = cacheStoragePolicty
-    }
-    public init(result: Result<(data: Data, response: HTTPURLResponse), ResponseError>, cacheStoragePolicty: URLCache.StoragePolicy = defaultCacheStoragePolicy) {
-        self.result = result
-        self.cacheStoragePolicty = cacheStoragePolicty
-    }
+internal protocol ResponseType {
+    associatedtype Response: URLResponse
+    var result: Result<(data: Data, response: Response), ResponseError> { get }
 }
--- Sources/XChanger/Request.swift
+++ Sources/XChanger/Request.swift
@@ -1,18 +1,5 @@
 import Foundation
 
-internal protocol Request {
+internal protocol RequestType {
     var filter: RequestFilter { get }
-}
-
-public struct HTTPRequest: Request {
-    internal let httpMethod: String?
-    internal let filter: RequestFilter
-    
-    public init(
-        httpMethod: String? = nil,
-        filter: RequestFilter = .init()
-    ) {
-        self.httpMethod = httpMethod
-        self.filter = filter
-    }
 }
