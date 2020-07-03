37e97cba5d407db14cbd0faa5c5ab8fd32705c6f
bannzai <kingkong999yhirose@gmail.com>
2020-07-03 12:40:04 +0900

    :recycle: Rename to User

--- Tests/XChangerTests/XChangerTests.swift
+++ Tests/XChangerTests/XChangerTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import XChanger
 
-struct Coder: Codable, Equatable {
+struct User: Codable, Equatable {
     let id: Int
     let name: String
 }
@@ -15,7 +15,7 @@ final class XChangerTests: XCTestCase {
             XChanger.register()
             defer { XChanger.unregister() }
             
-            let json = try! JSONEncoder().encode(Coder(id: 10, name: "bannzai"))
+            let json = try! JSONEncoder().encode(User(id: 10, name: "bannzai"))
             XChanger.add(
                 XChanger.exchange().request(url: "/").response(data: json, statusCode: 200)
             )
@@ -29,8 +29,8 @@ final class XChangerTests: XCTestCase {
                 }
                 XCTAssertEqual(httpResponse.statusCode, 200)
                 
-                let decoded = try! JSONDecoder().decode(Coder.self, from: data!)
-                XCTAssertEqual(decoded, Coder(id: 10, name: "bannzai"))
+                let decoded = try! JSONDecoder().decode(User.self, from: data!)
+                XCTAssertEqual(decoded, User(id: 10, name: "bannzai"))
                 
                 expect.fulfill()
             }.resume()
@@ -63,8 +63,8 @@ final class XChangerTests: XCTestCase {
             XChanger.register()
             defer { XChanger.unregister() }
             
-            let json1 = try! JSONEncoder().encode(Coder(id: 10, name: "bannzai"))
-            let json2 = try! JSONEncoder().encode(Coder(id: 100, name: "kingkong999"))
+            let json1 = try! JSONEncoder().encode(User(id: 10, name: "bannzai"))
+            let json2 = try! JSONEncoder().encode(User(id: 100, name: "kingkong999"))
             XChanger.add(
                 XChanger.exchange().request(url: "http://com.bannzai.xchanger/v1/user/10").response(data: json1, statusCode: 200),
                 XChanger.exchange().request(url: "http://com.bannzai.xchanger/v1/user/100").response(data: json2, statusCode: 200)
@@ -80,8 +80,8 @@ final class XChangerTests: XCTestCase {
                     }
                     XCTAssertEqual(httpResponse.statusCode, 200)
                     
-                    let decoded = try! JSONDecoder().decode(Coder.self, from: data!)
-                    XCTAssertEqual(decoded, Coder(id: 10, name: "bannzai"))
+                    let decoded = try! JSONDecoder().decode(User.self, from: data!)
+                    XCTAssertEqual(decoded, User(id: 10, name: "bannzai"))
                     
                     expect.fulfill()
                 }.resume()
@@ -97,8 +97,8 @@ final class XChangerTests: XCTestCase {
                     }
                     XCTAssertEqual(httpResponse.statusCode, 200)
                     
-                    let decoded = try! JSONDecoder().decode(Coder.self, from: data!)
-                    XCTAssertEqual(decoded, Coder(id: 100, name: "kingkong999"))
+                    let decoded = try! JSONDecoder().decode(User.self, from: data!)
+                    XCTAssertEqual(decoded, User(id: 100, name: "kingkong999"))
                     
                     expect.fulfill()
                 }.resume()
