b1f673776b864d2299c06bf94e97210af6d6f6ed
bannzai <kingkong999yhirose@gmail.com>
2020-07-03 23:02:32 +0900

    :sparkles: add struct for HTTPRequest

--- Sources/XChanger/Request.swift
+++ Sources/XChanger/Request.swift
@@ -7,24 +7,35 @@ internal func defaultCanInit(with request: URLRequest) -> Bool { true }
 internal func defaultCanonicalRequest(for request: URLRequest) -> URLRequest { request }
 internal func defaultRequestIsCacheEquivalent(a: URLRequest, b: URLRequest) -> Bool { false }
 
-public struct Request {
-    internal let httpMethod: String?
+public struct RequestFilter {
     internal let filter: ((URLRequest, URLRequest) -> Bool)
     internal let canInit: ((URLRequest) -> Bool)
     internal let canonicalRequest: ((URLRequest) -> URLRequest)
     internal let requestIsCacheEquivalent: ((URLRequest, URLRequest) -> Bool)
     
     public init(
-        httpMethod: String? = nil,
         filter: ((URLRequest, URLRequest) -> Bool)? = nil,
         canInit: ((URLRequest) -> Bool)? = nil,
         canonicalRequest: ((URLRequest) -> URLRequest)? = nil,
         requestIsCacheEquivalent: ((URLRequest, URLRequest) -> Bool)? = nil
     ) {
-        self.httpMethod = httpMethod
         self.filter = filter ?? defaultRequestFilter(a:b:)
         self.canInit = canInit ?? defaultCanInit(with:)
         self.canonicalRequest = canonicalRequest ?? defaultCanonicalRequest(for:)
         self.requestIsCacheEquivalent = requestIsCacheEquivalent ?? defaultRequestIsCacheEquivalent(a:b:)
+    }
+    
+}
+
+public struct HTTPRequest {
+    internal let httpMethod: String?
+    internal let filter: RequestFilter
+    
+    public init(
+        httpMethod: String? = nil,
+        filter: RequestFilter = .init()
+    ) {
+        self.httpMethod = httpMethod
+        self.filter = filter
     }
 }
